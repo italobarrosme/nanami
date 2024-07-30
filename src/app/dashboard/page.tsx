fdfacdae7763032bd441e14f69a7308702a3cb2f
italo barros <italobarros.me@gmail.com>
2024-07-30 17:38:51 -0300

    feat: regulary routes

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Text } from '@developerskyi/react-components'
+
+export default async function Dashboard() {
+  return (
+    <>
+      <section className="flex max-w-5xl flex-col gap-16 text-primary-regular">
+        <Text>Dashboard</Text>
+      </section>
+    </>
+  )
+}
